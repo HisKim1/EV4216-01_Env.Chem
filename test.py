fd66613615999398c60a438f655f9d97b9b9286d
HisKim1 <tomm1203@gist.ac.kr>
2023-03-25 16:55:52 +0900

    2023_03_25 data

---
+++ test.py
@@ -0,0 +1,15 @@
+def test():
+    print("hello")
+    return
+
+def loadfile():
+    text = open("test.txt", "r")
+    print(text.read())
+    return
+
+
+    
+
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+    test()
